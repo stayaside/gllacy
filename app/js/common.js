be3cca2e6f450cf93c1b73972fce92bfd51a8c31
stayacid <stayacid@gmail.com>
2018-01-16 13:46:02 +0300

    Finished disable, trying to correct social buttons

--- app/js/common.js
+++ app/js/common.js
@@ -1,27 +1,39 @@
-$( function(){
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 1000,
-      step: 10,
-      values: [ 100, 500 ],
-      slide: function( event, ui ) {
-        $( "#amount-start" ).val(ui.values[ 0 ] + "₽");
-        $( "#amount-end" ).val(ui.values[ 1 ] + "₽");
-      }
-    });
-    $( "#amount-start" ).val( $( "#slider-range" ).slider( "values", 0 ) + 
-      "₽");
-    $( "#amount-end" ).val( $( "#slider-range" ).slider( "values", 1 ) + 
-      "₽");
+var link = document.querySelector(".callback-button");
+var popup = document.querySelector(".callback-popup");
+var close = popup.querySelector(".callback-popup-close");
+var user = popup.querySelector("[name=name]");
+var form = popup.querySelector("form");
+var mail = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=textarea]");
+
+
+//LOGIN
+
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-content-show");
+    user.focus();
 });
 
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop:false,
-    items:1,
-    dots: true,
-    nav:true,
-    center: true
-  });
+close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+    popup.classList.remove("modal-error");
+}); 
+
+form.addEventListener("submit", function(event) {
+    if (!user.value || !mail.value || !text.value) {
+        event.preventDefault();
+        popup.classList.add("modal-error");
+    } else {
+        localStorage.setItem("user", user.value);
+    }
+});  
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (popup.classList.contains("modal-content-show")) {
+            popup.classList.remove("modal-content-show");
+        }
+    }
 });
